8b9855ab737301e6d12871e4fe589c13a0cb44a0
zoid <zoid@zoidmarketing.com>
2014-08-11 15:08:31 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'd52f2841436739477186d2e0a439bdb40e148922998ad354bc981e2df48c3b2f45c3213a836672ef8e8bc3458e42c34963e6dd0e5f43ef2a8ee56dd28ee73c13'
